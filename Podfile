150e4c8aae9fb183269341561fc51d3724025128
Appinventiv <aishwarya.rastogi@appinventiv.com>
2017-02-27 19:53:47 +0530

    commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'facebookLogin' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for facebookLogin
+
+
+
+
+pod ‘FBSDKCoreKit'
+pod ‘FBSDKShareKit’
+pod ‘FBSDKLoginKit’
+
+
+
+
+end
--- facebookLogin/ViewController.swift
+++ facebookLogin/ViewController.swift
@@ -7,18 +7,35 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let login = FBSDKLoginButton()
+        login.center = self.view.center
+       // login.frame = CGRect(x: 16, y: 15, width: view.frame.width - 32, height: 50)
+        self.view.addSubview(login)
+      
+        
+    }
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+          print( error)
+        }
+        else {
+            print("successful login")
+        }
+    }
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("logrd out successfully")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
